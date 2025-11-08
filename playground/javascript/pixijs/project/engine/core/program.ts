b1128192d63ff20fefdcbd8211c9159d97a2c46b
Cihan Eran <erancihan@gmail.com>
2025-11-08 14:07:59 +0300

    feat: update pixijs mock

--- playground/javascript/pixijs/project/engine/core/program.ts
+++ playground/javascript/pixijs/project/engine/core/program.ts
@@ -1,42 +1,11 @@
-import { Application, Container, type ApplicationOptions } from "pixi.js";
+import { Application, type ApplicationOptions } from "pixi.js";
 
-import { createActor } from "../utils/create-actor";
-
-class Scene extends Container {
-  private actors: any[] = [];
-
-  initalize() {
-    this.actors.forEach((actor) => actor?.init?.());
-  }
-
-  update(dt: number) {
-    this.actors.forEach((actor) => actor?.update?.(dt));
-  }
-
-  setActors(actors: any[]) {
-    actors.forEach((data) => {
-      const actor = createActor(data);
-      if (!actor) {
-        console.warn(
-          `Failed to create actor for data: ${JSON.stringify(data)}`
-        );
-        return;
-      }
-
-      this.addChild(actor);
-      this.actors.push(actor);
-    });
-
-    actors.forEach((actor) => actor?.resolveSlots?.());
-  }
-}
+import { Scene } from "./scene";
 
 class Program {
   public app: Application;
 
   private scene: Scene | null = null;
-
-  private actors: any[] = [];
 
   constructor() {
     this.app = new Application();
--- playground/javascript/pixijs/project/engine/utils/create-actor.ts
+++ playground/javascript/pixijs/project/engine/utils/create-actor.ts
@@ -1,9 +1,9 @@
 import { Circle } from "../actors/circle.js";
 
-function createActor(entity: any) {
+function createActor(entity: any, scene: any): any {
   switch (entity.type) {
     case "circle":
-      return new Circle(entity);
+      return new Circle(entity, scene);
     default:
       console.warn(`Unknown actor type: ${entity.type}`);
       return null;
